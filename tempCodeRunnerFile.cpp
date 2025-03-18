7f2bdd4d9647492a72c398ad64ad1b1f824a0ccc
Yaswanth876 <vsyaswanth008@gmail.com>
2025-03-18 22:26:14 +0530

    changes

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-R, C = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(R)]
-N = int(input())
-for i in range(N, R):
-    for j in range(C):
-        matrix[i - N][j] += matrix[i][j]
-for i in range(R - N):
-    print(*matrix[i])
